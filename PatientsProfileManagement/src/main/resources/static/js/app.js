8f60c8ebd3b4abebfce2d232f7419a908c081165
wisteriaTTN <tranthiennhu1904@gmail.com>
2017-03-08 21:24:58 +0700

    aaa

--- PatientsProfileManagement/src/main/resources/static/js/app.js
+++ PatientsProfileManagement/src/main/resources/static/js/app.js
@@ -1,11 +1,15 @@
 var app = angular.module('myApp', ['ngRoute','ngResource']);
 app.config(function($routeProvider){
     $routeProvider
-	    .when('/another',{
+	    /*.when('/another',{
 	        templateUrl: '/patientprofile/views/another.html',
 	        controller: 'AnotherController'
-	    })
-        .otherwise(
+	    })*/
+	    .when('/',{
+	        templateUrl: '/views/homepage.html',
+	        controller: 'HomeController'
+	    });
+        /*.otherwise(
             { redirectTo: '/'}
-        );
+        );*/
 });
