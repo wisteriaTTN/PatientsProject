8e8772479caba6158afc86f5e71e327174e77aee
TranThienNhu <tranthiennhu1904@gmail.com>
2017-03-08 16:01:59 -0500

    homepage

--- PatientsProfileManagement/src/main/resources/static/js/handle.js
+++ PatientsProfileManagement/src/main/resources/static/js/handle.js
@@ -2,5 +2,24 @@
  * 
  */
 ( function() {
+	new WOW().init();
+	
+	var $item = $('.carousel .item'); 
+	var $wHeight = $(window).height();
+	$item.eq(0).addClass('active');
+	$item.height($wHeight); 
+	$item.addClass('full-screen');
+
+
+	$(window).on('resize', function (){
+	  $wHeight = $(window).height();
+	  $item.height($wHeight);
+	});
+
+	$('.carousel').carousel({
+	  interval: 6000,
+	  pause: "false"
+	});
+	
 	
 } );
