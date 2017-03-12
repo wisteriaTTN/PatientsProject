805ebfbd9bdd3d84ad9c87cee081a39c00eb9e52
ngxdiep <ngxdiep@gmail.com>
2017-03-13 00:14:17 +0700

    commit change

---
+++ PatientsProfileManagement/src/main/java/com/csc/team2/controller/TypeMedicineController.java
@@ -0,0 +1,49 @@
+package com.csc.team2.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.csc.team2.model.TypeMedicine;
+import com.csc.team2.service.TypeMedicineServiceImpl;
+
+@Controller
+public class TypeMedicineController {
+	
+//	@Autowired
+//	TypeMedicineServiceImpl typeMedicineService;
+//	
+//	public static final Logger logger = LoggerFactory.getLogger(TypeMedicineController.class);
+//	
+//	//--------------------Select All TypeMedicine-----------------------------------------
+//	
+//	@RequestMapping(value="/typemedicine", method = RequestMethod.GET)
+//	public ResponseEntity<List<TypeMedicine>> listMedicine(){
+//		List<TypeMedicine> typeMedicines = typeMedicineService.findAllTypeMedicines();
+//		if(typeMedicines.isEmpty()){
+//			return new ResponseEntity(HttpStatus.NO_CONTENT);
+//		}
+//		return new ResponseEntity<List<TypeMedicine>>(typeMedicines, HttpStatus.OK);
+//	}
+//	
+//	//--------------------Select One TypeMedicine-----------------------------------------
+//	@RequestMapping(value="/typemedicine/{id}", method = RequestMethod.GET)
+//	public ResponseEntity<?> getMedicine(@PathVariable("id") int id){
+//		logger.info("Fetching Medicine with id {}", id);
+//		TypeMedicine typeMedicine = typeMedicineService.findById(id);
+//		if(typeMedicine==null){
+//			 logger.error("Patient with id {} not found.", id);
+//			 return new ResponseEntity(HttpStatus.NOT_FOUND);
+//		}
+//		return new ResponseEntity<TypeMedicine>(typeMedicine, HttpStatus.OK);
+//	}
+
+}
---
+++ PatientsProfileManagement/src/main/java/com/csc/team2/service/AllergicServiceImpl.java
@@ -0,0 +1,57 @@
+package com.csc.team2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csc.team2.model.Allergic;
+import com.csc.team2.repository.IAllergicRepository;
+
+@Service
+public class AllergicServiceImpl implements IAllergicService {
+	
+	@Autowired
+	IAllergicRepository allergicRepos;
+
+	@Override
+	public Allergic findById(int id) {
+		return allergicRepos.findOne(id);
+	}
+
+	@Override
+	public void saveAllergic(Allergic allergic) {
+		allergicRepos.save(allergic);
+		
+	}
+
+	@Override
+	public void updateAllergic(Allergic allergic) {
+		saveAllergic(allergic);
+		
+	}
+
+	@Override
+	public void deleteAllergicById(int id) {
+		allergicRepos.delete(id);
+		
+	}
+
+	@Override
+	public void deleteAllAllergics() {
+		allergicRepos.deleteAll();
+		
+	}
+
+	@Override
+	public List<Allergic> findAllAllergics() {
+		return allergicRepos.findAll();
+	}
+
+	@Override
+	public boolean isAllergicExist(Allergic allergic) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
