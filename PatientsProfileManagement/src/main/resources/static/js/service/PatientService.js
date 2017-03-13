fd273f9147191649e38cc117232c3297b40f7428
Diep <ngxdiep@gmail.com>
2017-03-13 18:05:10 -0400

    patient Crud

--- PatientsProfileManagement/src/main/resources/static/js/service/PatientService.js
+++ PatientsProfileManagement/src/main/resources/static/js/service/PatientService.js
@@ -1,12 +1,19 @@
 var app = angular.module('myApp');
 
-app.factory('storeService', function($http){
+app.factory('patientService', function($http){
   var getPatient = function() {
 	    return $http.get("http://localhost:8080/patient")
 	      .then(function(response) {
 	        return response.data;
 	      });
   };
+  
+  var getOnePatient = function(id){
+	  return $http.get("http://localhost:8080/patient/" + id)
+	  .then(function(response){
+			return response.data;
+		});
+  }
   
   var getDetail = function(id) {
 	  return $http.get("http://localhost:8080/patient/" + id)
@@ -16,16 +23,47 @@ app.factory('storeService', function($http){
   }
   var getPatientTrament = function(id) {
 	  return $http.get("http://localhost:8080/patient/" + id)
-	  			.then(function(response){
-					return response.data;
-				});
+	  .then(function(response){
+			return response.data;
+		});
+  }
+  
+  var createPatient = function(patient){
+	  return $http.post("http://localhost:8080/patient",patient)
+	  .then(function(response){
+			return response.data;
+		});
+  }
+  var updatePatient = function(id,patient){
+	  return $http.put("http://localhost:8080/patient/"+id,patient)
+	  .then(function(response){
+			return response.data;
+		});
+  }
+  
+  var deletePatient = function(id){
+	  return $http.delete("http://localhost:8080/patient/"+id)
+	  .then(function(response){
+			return response.data;
+		});
+  }
+  
+  var deleteAllpatient = function(){
+	  return $http.delete("http://localhost:8080/patient")
+	  .then(function(response){
+			return response.data;
+		});
   }
   
   return {
-	  getProduct: getProduct,
-	  add: add,
-	  increaseData: increaseData,
+	  deletePatient: deletePatient,
+	  getPatient: getPatient,
   	  getDetail: getDetail,
-  	  decreaseData:decreaseData,
+  	  createPatient: createPatient,
+  	  updatePatient: updatePatient,
+  	  deleteAllpatient: deleteAllpatient,
+  	  getOnePatient:getOnePatient,
   }
 });
+
+
