3a0928fc4fcb31e6a30c0b87bea14d08237e3699
Nam <thenamcd@gmail.com>
2017-03-13 10:14:11 -0400

    add

--- PatientsProfileManagement/src/main/java/com/csc/team2/DemoApplication.java
+++ PatientsProfileManagement/src/main/java/com/csc/team2/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.csc.team2;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.csc.team2.controller.UploadingController;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		new File(UploadingController.uploadingdir).mkdirs();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
