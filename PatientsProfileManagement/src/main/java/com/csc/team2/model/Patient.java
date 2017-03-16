1fc23aa021639d98decb14ef8d147d0ecea78841
wisteriaTTN <tranthiennhu1904@gmail.com>
2017-03-16 20:16:27 +0700

    commit 8h16

--- PatientsProfileManagement/src/main/java/com/csc/team2/model/Patient.java
+++ PatientsProfileManagement/src/main/java/com/csc/team2/model/Patient.java
@@ -166,51 +166,110 @@ public class Patient implements Serializable {
 		this.name = name;
 	}
 
-	public String getAddress() {
-		return address;
-	}
+	package com.csc.team2.model;
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+	import java.io.Serializable;
+	import java.util.Date;
 
-	public String getSex() {
-		return sex;
-	}
+	import javax.persistence.Column;
+	import javax.persistence.Entity;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
+	import javax.persistence.Table;
+	import javax.persistence.Temporal;
+	import javax.persistence.TemporalType;
 
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
+	import org.hibernate.annotations.Type;
 
-	public Date getDob() {
-		return dob;
-	}
 
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
 
-	public Patient(Integer patienId, String name, String address, String sex, Date dob) {
-		this.patienId = patienId;
-		this.name = name;
-		this.address = address;
-		this.sex = sex;
-		this.dob = dob;
+
+	@Entity
+	@Table(name="patient")
+	public class Patient implements Serializable {
+		
+		private static final long serialVersionUD =1L;
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name="id")
+		private Integer patienId;
+		
+		@Column(name="name")
+		private String name;
+		
+		@Column(name="address")
+		private String address;
+		
+		@Column(name="sex")
+		private String sex;
+		
+		@Temporal(TemporalType.DATE)
+		@Column(name="dob")
+		private Date dob;
+		
+		public Integer getPatienId() {
+			return patienId;
+		}
+
+		public void setPatienId(Integer patienId) {
+			this.patienId = patienId;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public String getSex() {
+			return sex;
+		}
+
+		public void setSex(String sex) {
+			this.sex = sex;
+		}
+
+		public Date getDob() {
+			return dob;
+		}
+
+		public void setDob(Date dob) {
+			this.dob = dob;
+		}
+
+		public Patient(Integer patienId, String name, String address, String sex, Date dob) {
+			this.patienId = patienId;
+			this.name = name;
+			this.address = address;
+			this.sex = sex;
+			this.dob = dob;
+		}
+		
+		public Patient(Patient patient){
+			this.patienId = patient.patienId;
+			this.name = patient.name;
+			this.address = patient.address;
+			this.sex = patient.sex;
+			this.dob = patient.dob;
+		}
+		public Patient()
+		{
+			this.patienId = 0;
+			this.name = "";
+			this.address = "";
+			this.sex = "";
+			this.dob = null;
+		}
 	}
-	
-	public Patient(Patient patient){
-		this.patienId = patient.patienId;
-		this.name = patient.name;
-		this.address = patient.address;
-		this.sex = patient.sex;
-		this.dob = patient.dob;
-	}
-	public Patient()
-	{
-		this.patienId = 0;
-		this.name = "";
-		this.address = "";
-		this.sex = "";
-		this.dob = null;
-	}
-}
