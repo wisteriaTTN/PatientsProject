4cfc4c7475ef0bbaf5deb3d79a9171ae0ec42d75
wisteriaTTN <tranthiennhu1904@gmail.com>
2017-03-17 00:25:09 +0700

    12h25 17/3

--- PatientsProfileManagement/src/main/resources/static/js/scripts.js
+++ PatientsProfileManagement/src/main/resources/static/js/scripts.js
@@ -9,13 +9,13 @@ jQuery(document).ready(function() {
     /*
         Form validation
     */
-    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    $('.login-form input[type="text"], .login-form input[type="password"]').on('focus', function() {
     	$(this).removeClass('input-error');
     });
     
     $('.login-form').on('submit', function(e) {
     	
-    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    	$(this).find('input[type="text"], input[type="password"]').each(function(){
     		if( $(this).val() == "" ) {
     			e.preventDefault();
     			$(this).addClass('input-error');
@@ -28,4 +28,21 @@ jQuery(document).ready(function() {
     });
     
     
+    $('.regis input[type="text"], .regis input[type="password"]').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.regis').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"]').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
 });
