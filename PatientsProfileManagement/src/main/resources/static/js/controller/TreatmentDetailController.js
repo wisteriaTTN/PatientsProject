a4f65d3175f0c095765c01344b07a7f31f5cf3b4
Diep <ngxdiep@gmail.com>
2017-03-21 04:07:06 +0700

    add treatment treatmentdetail

--- PatientsProfileManagement/src/main/resources/static/js/controller/TreatmentDetailController.js
+++ PatientsProfileManagement/src/main/resources/static/js/controller/TreatmentDetailController.js
@@ -1,15 +1,139 @@
 var app = angular.module('myApp');
 app.controller('treatmentDetailController', function(
-        $scope, $interval, $location,$http,$routeParams) {
+        $scope, $interval, $location,$http,$routeParams,treatmentDetailService,allergicService) {
 	
 	
 	
-	$scope.treatment=[];
+	$scope.treatment;
+	
+	$scope.allergic={
+			medicineId:{
+				
+			},
+			patientId:{
+				
+			}
+	}
+	
+	$scope.treatmentDetail={
+			treatmentId:{
+					
+			},
+			medicineId:{
+				
+			},
+			diseases:null
+			
+	}
 	
 	$http.get("http://localhost:8080/treatment/" +$routeParams.treatmentId).then(function(response) {
 		$scope.treatment = response.data;
 		$scope.treatment.patientId.dob = new Date(response.data.patientId.dob);
 	});
+	
+	
+////=========Create allergics list======================================
+	$scope.createAllergics = function(){
+		if($scope.allergics!=null){
+			angular.forEach($scope.allergics,function(value, key){
+				$scope.allergic.medicineId = value;
+				$scope.allergic.patientId  =  $scope.treatment.patientId;
+				allergicService.createAllergic($scope.allergic).then(createAllergicSuccess,createAllergicError)
+			});
+		}
+		else{
+			
+		}
+	}
+	var createAllergicSuccess = function(data) {
+		alert('add new treatment Success:');
+	};
+	var createAllergicError = function(error) {
+	};
+	
+	
+////========create treatment deetail=================================	
+	$scope.createTreatmentDetail = function(){
+		if($scope.medicines!=null){
+			angular.forEach($scope.medicines, function(value, key){
+				$scope.treatmentDetail.treatmentId = $scope.treatment;
+				$scope.treatmentDetail.medicineId =value;
+				$scope.treatmentDetail.diseases =$scope.treatment.prescription;
+				treatmentDetailService.createTreatmentDetail($scope.treatmentDetail).then(createTreatmentDetailSuccess,createTreatmentDetailError)
+				
+			});
+		}
+		else{
+			
+		}
+	}
+	
+	var createTreatmentDetailSuccess = function(data) {
+		alert('add new treatment Success:');
+	};
+	var createTreatmentDetailError = function(error) {
+		alert("wrong hreee");
+	};
+	
+////==========delete treatment detail=================================
+	$scope.deleteTreatmentDetail = function(id){
+		treatmentDetailService.deleteTreatmentDetail(id).then(deleteTreatmentDetailSuccess,deleteTreatmentDetailError)
+	}
+	var deleteTreatmentDetailSuccess = function(data) {
+		alert('delete treatment Success:');
+	};
+	var deleteTreatmentDetailError = function(error) {
+	};
+	
+////==========delete allergic==========================================
+	$scope.deleteAllergic = function(id){
+		allergicService.dleteAllergic(id).then(deleteAllergicSuccess,deleteAllergiclError)
+	}
+	var deleteAllergicSuccess = function(data) {
+		alert('delete allergic Success:');
+	};
+	var deleteAllergiclError = function(error) {
+	};
+	
+////==========contact chips for prescription and allergics=============	
+    $scope.querySearch = querySearch;
+    $scope.medicines = [];
+	$scope.allergics = [];
+	$scope.filterSelected = true;
+
+    function querySearch (query) {
+      var result;
+      if(query) {
+        result = loadAndParseContacts().then(function(data) {
+             return data.filter(createFilterFor(query))
+           })
+      } else {
+        result = []
+      }
+      return result
+    }
+
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(contact) {
+        return (contact._lowername.indexOf(lowercaseQuery) != -1);;
+      };
+
+    }
+    
+    function parse (data) {
+      return data.data.map(function (c, index) {
+          var contact = {
+            name: c.name,
+            id: c.id,
+          };
+          contact._lowername = contact.name.toLowerCase();
+          return contact;
+        })      
+    }
+    function loadAndParseContacts() {
+      return $http.get('http://localhost:8080/medicine').then(parse)
+    }
 
 });
 //
